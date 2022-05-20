67be82e26d9ad8d7bc028fb7ebaab184385f78b2
KalininAleksey <a.kalinin.study@gmail.com>
2022-05-20 18:28:53 +0300

    Lab3 with Ex1,2 is added

---
+++ ITMO.Studing.CScourse/ITMO.Studing.CScourse.Lab3.Ex2.WhatDay2/WhatDay.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.Studing.CScourse.Lab3.Ex2.WhatDay2
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    class WhatDay
+    {
+        static System.Collections.ICollection DaysInMonths
+    = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Please enter a day number between 1 and 365: ");
+                string line = Console.ReadLine();
+                int dayNum = int.Parse(line);
+                if (dayNum < 1 || dayNum > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+                int monthNum = 0;
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught);
+            }
+         
+        }
+    }
+}
