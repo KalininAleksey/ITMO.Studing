6d8884b50566d7dee6baeafc5b0732308c1d0376
KalininAleksey <a.kalinin.study@gmail.com>
2022-05-22 23:03:29 +0300

    Lab4.Ex1 is added

---
+++ ITMO.Studing.CScourse/ITMO.Studing.CScourse.Lab4.Ex1.Utils/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.Studing.CScourse.Lab4.Ex1.Utils
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            int x; // Input value 1
+            int y; // Input value 2
+            int greater; // Result from Greater()
+
+            // Get input numbers
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+
+            // Test the Greater( ) method
+            greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is " + greater);
+        }
+    }
+
+
+}
---
+++ ITMO.Studing.CScourse/ITMO.Studing.CScourse.Lab4.Ex1.Utils/Utills.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.Studing.CScourse.Lab4.Ex1.Utils
+{
+    class Utils
+    {
+        //
+        // Return the greater of two integer values
+        //
+
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+
+}
