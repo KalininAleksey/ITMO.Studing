8c86ae5101c7a5753c6da5e716449e3a647648bd
KalininAleksey <a.kalinin.study@gmail.com>
2022-05-27 16:39:23 +0300

    Lab6Ex2 is added

---
+++ ITMO.Studing.CScourse/ITMO.Studing.CScourse.Lab6.Ex2.UniqueNumbers/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.Studing.CScourse.Lab6.Ex2.UniqueNumbers
+{
+    public enum AccountType { Checking, Deposit }
+    class BankAccount
+    {
+        public void Populate(decimal balance)
+        {
+            accNo = NextNumber();
+            accBal = balance;
+            accType = AccountType.Checking;
+        }
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+        public string Type()
+        {
+            return accType.ToString();
+        }
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        private static long nextAccNo = 123;
+    }
+}
