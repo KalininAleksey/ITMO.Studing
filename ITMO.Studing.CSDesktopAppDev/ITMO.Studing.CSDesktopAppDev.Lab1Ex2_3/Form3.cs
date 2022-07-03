acf15a27b23226246ee649bc3b92c01b28a43bc2
KalininAleksey <a.kalinin.study@gmail.com>
2022-07-03 14:35:39 +0300

    CSDesktopAppDev solution with Lab1 Exercise 1-5, Lab2 Exersise 1,2 is added

---
+++ ITMO.Studing.CSDesktopAppDev/ITMO.Studing.CSDesktopAppDev.Lab1Ex2_3/Form3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ITMO.Studing.CSDesktopAppDev.Lab1Ex2
+{
+    public partial class Form3 : ITMO.Studing.CSDesktopAppDev.Lab1Ex2.Form1
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
